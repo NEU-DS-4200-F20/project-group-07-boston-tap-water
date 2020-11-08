d021d8e9f49ab57f0c9e069362cd75af24ca34ee
emily-chen8 <61168499+emily-chen8@users.noreply.github.com>
2020-11-07 23:14:46 -0500

    comments done!

--- js/barchart.js
+++ js/barchart.js
@@ -2,7 +2,8 @@
 
 
 
-
+// Initialize a bar chart. Modeled after Mike Bostock's
+// Reusable Chart framework https://bost.ocks.org/mike/chart/
 function barChart(){
 
     let xValue = d => d[0],
@@ -13,186 +14,179 @@ function barChart(){
     yLabelOffsetPx = 0
     xScale = d3.scaleBand(),
     yScale = d3.scaleLinear();
-    //ourBrush = null,
-    //selectableElements = d3.select(null),
-    //dispatcher;
     let margin = {
       top: 100,
       bottom: 30,
       left: 90,
       right: 30
     };
-  // first visualization
-
-  let width = 1000; //adjusts the dimensions for second visualization
+  let width = 1000; 
   let height = 550;
 
+  // Create the chart by adding an svg to the div with the id 
+  // specified by the selector using the given data
+  function chart(selector, data){
+    //loads the data into the console
+    console.log(data); 
+    
+    //finds the maximum date for x axis
+    let maxDate  = d3.max(data, d => xValue(d)); 
+    //finds the minimum date for x axis
+    let minDate  = d3.min(data, d => xValue(d)); 
+     // finds the max meantemp for y axis
+    let maxCount = d3.max(data, d => y1Value(d));
+    
+    //prints out these values in console
+    console.log(maxDate, minDate, maxCount, d3.max(data, d => y2Value(d))); 
 
-function chart(selector, data){
-  console.log(data); //loads the data into the console
-  let maxDate  = d3.max(data, d => xValue(d)); //finds the maximum date for x axis
-  let minDate  = d3.min(data, d => xValue(d)); //finds the minimum date for x axis
-  let maxCount = d3.max(data, d => y1Value(d)); // finds the max meantemp for y axis
-  console.log(maxDate, minDate, maxCount, d3.max(data, d => y2Value(d))); //prints out these values in console
-
-  let svg2 = d3.select('#barChart')
-  .append('svg')
-  .attr('preserveAspectRatio', 'xMidYMid meet') // this will scale your visualization according to the size of the page.
-  .attr('width', '100%') // this is now required by Chrome to ensure the SVG shows up at all
-  .style('background-color', '#fff') // change the background color to white
-  .attr('viewBox', [0, 0, width + margin.left + margin.right , height + margin.top + margin.bottom].join(' '))
-
-
-
-
-  yScale
-  .domain([0,maxCount]) //sets the y axis to scale humidity from min to max
-  .range([height - margin.bottom, margin.top]); //formats the axis so it is between the margins
-  /*.domain([
-    d3.min(data, d => y1Value(d)),
-    d3.max(data, d => y1Value(d))
-  ])
-  .rangeRound([height, 0]);*/
-
-  xScale
-  .domain(
-    data.map(function(d) {
-      return xValue(d);
-    }) // makes the x values the dates
-  )
-  .range([margin.left, width - margin.right]) //scales it to fit between the left and right margins
-  .padding(0.5);
-
-  /*.domain(d3.group(data, xValue).keys())
-  .rangeRound([0, width]);*/
-
-//Draw Axes
-let yAxis = svg2
-  .append('g')
-  .attr('transform', `translate(${margin.left},0)`) //positions the y axis on the left margin
-  .style("font", "20px times")
-  .call(d3.axisLeft().scale(yScale))
-
-  //Add label
-  .append('text')
-  .attr('y', -60) // gives y coordinate of label
-  .attr('x', -195) //gives x coordinate of label
-  .style('fill', 'black') //styles the text to black
-  .attr('font-size', 25)
-  .attr("transform", "rotate(270)")
-  .text('Cell Count (cells/mL)') //adds Humidity as axis label
-
-
-let xAxis = svg2
-  .append('g')
-  .attr('transform', `translate(0,${height - margin.bottom})`) //positions the x axis on the bottom margin
-  .style("font", "20px times")
-  .call(d3.axisBottom().scale(xScale))
-
-  //Add label
-  .append('text')
-  /*.attr('x', width2 - margin.left) //gives x coordinate of label to left margin
-  .attr('y', -10)// gives y coordinate of lable
-  .style('stroke', 'black')*/
-  .attr('x', 500) //gives x coordinate of label to left margin
-  .attr('y', 70)// gives y coordinate of lable
-  .attr('font-size', 25)
-  .style('fill', 'black') //styles the text to black
-
-  .text('Dates'); //adds Dates as text
-
-
-let title = svg2
-  .append('g')
-  .append('text')
-  .attr('x', 380) //gives x coordinate of label to left margin
-  .attr('y', 25)// gives y coordinate of lable
-  .style('fill', 'black') //styles the text to black
-  .style("font", "30px times")
-  .style("font-weight", "bold")
-  .text('Cell Count Over Time'); //adds Dates as text
-
-
-//Draw bars
-
-let g = svg2.selectAll(".rect")
-  .data(data)
-  .enter()
-  .append("g")
-  .classed('rect', true)
-
-
-
-g.append("rect")
-  .attr('x', function(d) {
-    return X(d); //makes the bars by date
-  })
-  .attr('y', function(d) {
-    return Y1(d); //makes the y value humidity
-  })
-  .attr('width', xScale.bandwidth()) //spaces the bars along the xScale object
-  .attr('fill', '#9370DB') //fills in the bar according to colormap
-  .attr('height', function(d) {
-    //console.log(X(d))
-    return height - margin.bottom - Y1(d); //makes the height of the bar match its humidity
-  })
-
-
+    let svg2 = d3.select('#barChart')
+    .append('svg')
+    // scale visualization according to the size of the page.
+    .attr('preserveAspectRatio', 'xMidYMid meet') 
+    .attr('width', '100%') 
+    .style('background-color', '#fff') // change the background color to white
+    .attr('viewBox', [0, 0, width + margin.left + margin.right , height + margin.top + margin.bottom].join(' '))
+  
+    //Define scales
+    yScale
+    .domain([0,maxCount]) //sets the y axis to scale count from min to max
+    .range([height - margin.bottom, margin.top]); //formats the axis so it is 
+  
+    xScale
+    .domain(
+      data.map(function(d) {
+        return xValue(d);
+      }) // makes the x values the dates
+    )
+    .range([margin.left, width - margin.right]) //scales it to fit between the left and right margins
+    .padding(0.5);
+  
+  //Draw axis
+  let yAxis = svg2
+    .append('g')
+    .attr('transform', `translate(${margin.left},0)`) //positions the y axis on the left margin
+    .style("font", "20px times")
+    .call(d3.axisLeft().scale(yScale))
+  
+    //Add label
+    .append('text')
+    .attr('y', -60) // gives y coordinate of label
+    .attr('x', -195) //gives x coordinate of label
+    .style('fill', 'black') //styles the text to black
+    .attr('font-size', 25)
+    .attr("transform", "rotate(270)")
+    .text('Cell Count (cells/mL)') //adds cell count as axis label
+  
+  
+  let xAxis = svg2
+    .append('g')
+    //positions the x axis on the bottom margin
+    .attr('transform', `translate(0,${height - margin.bottom})`) 
+    .style("font", "20px times")
+    .call(d3.axisBottom().scale(xScale))
+  
+    //Add label
+    .append('text')
+    .attr('x', 500) //gives x coordinate of label to left margin
+    .attr('y', 70)// gives y coordinate of lable
+    .attr('font-size', 25)
+    .style('fill', 'black') //styles the text to black
+  
+    .text('Dates'); //adds Dates as text
+  
+  // Add a title to the chart
+  let title = svg2
+    .append('g')
+    .append('text')
+    .attr('x', 380) //gives x coordinate of label to left margin
+    .attr('y', 25)// gives y coordinate of lable
+    .style('fill', 'black') //styles the text to black
+    .style("font", "30px times")
+    .style("font-weight", "bold")
+    .text('Cell Count Over Time'); //adds Cell Count Over Time as text
+  
+  
+  //Draw bars
+  let g = svg2.selectAll(".rect")
+    .data(data)
+    .enter()
+    .append("g")
+    .classed('rect', true)
+  
+  // append top bar (light purple)
   g.append("rect")
-  .attr('x', function(d) {
-   return X(d); //makes the bars by date
-  })
-  .attr('y', function(d) {
-    return Y2(d); //makes the y value humidity
-  })
-  .attr('width', xScale.bandwidth()) //spaces the bars along the xScale object
-  .attr('fill', '#3A1D90') //fills in the bar according to colormap
-  .attr('height', function(d) {
-    return height - margin.bottom - Y2(d); //makes the y value humidity
-  })
-
-  colors = ['#9370DB', '#3A1D90']
-  values = [90, 113]
-
-let legend = svg2.selectAll(".legend")
-  .data(colors)
-  .enter().append("g")
-  .attr("class", "legend")
-  .attr("transform", function(d, i) { return "translate(30," + values[i] + ")"; });
-
-legend.append("rect")
-  .attr("x", width - 400 )
-  //.attr ("y", height2 + 20)
-  .attr("width", 18)
-  .attr("height", 18)
-  .style("fill", function(d, i) {return colors[i];});
-
-legend.append("text")
-  .attr("x", width - 375 )
-  .attr("y", 9)
-  .attr("dy", ".35em")
-  .style("text-anchor", "start")
-  .style("font", "20px times")
-  .text(function(d, i) {
-    switch (i) {
-      case 0: return "Membrane Non-Intact Cells";
-      case 1: return "Membrane Intact Cells";
-    }
-  });
-
-  return chart;
+    .attr('x', function(d) {
+      return X(d); //makes the bars by date
+    })
+    .attr('y', function(d) {
+      return Y1(d); //makes the y value humidity
+    })
+    .attr('width', xScale.bandwidth()) //spaces the bars along the xScale object
+    .attr('fill', '#9370DB') //fills in the bar according to colormap
+    .attr('height', function(d) {
+      return height - margin.bottom - Y1(d); //makes the height of the bar match its humidity
+    })
+  
+    // append bottom bar (darker purple)
+    g.append("rect")
+    .attr('x', function(d) {
+     return X(d); //makes the bars by date
+    })
+    .attr('y', function(d) {
+      return Y2(d); //makes the y value humidity
+    })
+    .attr('width', xScale.bandwidth()) //spaces the bars along the xScale object
+    .attr('fill', '#3A1D90') //fills in the bar according to colormap
+    .attr('height', function(d) {
+      return height - margin.bottom - Y2(d); //makes the y value humidity
+    })
+  
+    colors = ['#9370DB', '#3A1D90']
+    values = [90, 113]
+  
+  // append a legend onto the chart
+  let legend = svg2.selectAll(".legend")
+    .data(colors)
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function(d, i) { return "translate(30," + values[i] + ")"; });
+  
+  // append the boxes in the legenc
+  legend.append("rect")
+    .attr("x", width - 400 )
+    //.attr ("y", height2 + 20)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", function(d, i) {return colors[i];});
+  
+  // append the text to the legenc
+  legend.append("text")
+    .attr("x", width - 375 )
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "start")
+    .style("font", "20px times")
+    .text(function(d, i) {
+      switch (i) {
+        case 0: return "Membrane Non-Intact Cells";
+        case 1: return "Membrane Intact Cells";
+      }
+    });
+  
+    return chart;
 }
 
-
+  // The x-accessor from the datum
   function X(d) {
     return xScale(xValue(d));
   }
 
-  // The y-accessor from the datum
+  // The y1-accessor from the datum (Non-Intact cells)
   function Y1(d) {
     return yScale(y1Value(d));
   }
 
+  // The y2-accessor from the datum (Intact cells)
   function Y2(d) {
     return yScale(y2Value(d));
   }
